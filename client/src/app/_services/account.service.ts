bd4c6b23591ad79ab0aa9832a968926b876307f1
Abhishek Salgaonkar <abhisal12384@gmail.com>
2021-08-03 15:54:46 -0400

    commit toastr

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -49,5 +49,6 @@ export class AccountService {
   logout(){
     localStorage.removeItem('user');
     this.currentUserSource.next();
+    console.log(this.currentUser$)
   }
 }
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -8,7 +10,7 @@ import { AccountService } from '../_services/account.service';
 })
 export class NavComponent implements OnInit {
 
-  constructor(public accountService:AccountService) { }
+  constructor(public accountService:AccountService,private router:Router,private toastr: ToastrService) { }
   model:any = {};
   
   ngOnInit(): void {
@@ -16,15 +18,18 @@ export class NavComponent implements OnInit {
   }
   login(){
     this.accountService.login(this.model).subscribe(reponse => {
-      console.log(reponse)
+      this.router.navigateByUrl('/members');
       
     },error => {
-      console.log(error)
+      console.log(error);
+      this.toastr.error(error.error);
     })
    
   }
   logout(){ 
     this.accountService.logout();
+    this.router.navigateByUrl('/');
+      
   }
  
 }
